87268de91c6e3ba25125e7b645da2e0a1af8b67c
imaheshaher <maheshaher035@gmail.com>
2021-08-01 20:23:33 +0530

    alarm change

--- alarm.py
+++ alarm.py
@@ -5,10 +5,10 @@ from datetime import datetime
 import os
 alarm_time = "19:22:35"
 alarm_hour = alarm_time[0:2]
-alarm_minute = alarm_time[3:5]
+alarm_minutes = alarm_time[3:5]
 alarm_second = alarm_time[6:8]
 alarm_period = alarm_time[9:11].upper()
-print(alarm_hour,alarm_minute,alarm_second,alarm_period)
+print(alarm_hour,alarm_minutes,alarm_second,alarm_period)
 # print(os.getcwd()+ str("\song.mp3"))
 
 while True:
@@ -19,7 +19,7 @@ while True:
     current_period = now.strftime("%p")
     print(current_period)
     if current_hour==alarm_hour:
-        if current_minute==alarm_minute:
+        if current_minute==alarm_minutes:
             if current_second==alarm_second:
                 print("Wakd up..")
 
